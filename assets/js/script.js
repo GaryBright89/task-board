06d454aa04a0c6e57d935756ea90cab194857688
Gary Bright <garybright@rocketmail.com>
2024-03-14 19:58:59 -0400

    organized some style code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//DEPENDENCIES
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
@@ -8,6 +9,8 @@ const addTaskButton = document.querySelector(".btn btn-success");
 
 renderTaskList();
 
+
+//FUNCTIONS
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
   nextId++;
@@ -17,8 +20,8 @@ function generateTaskId() {
 // Todo: create a function to create a task card
 function createTaskCard(task) {
     const taskCard = $('<div>')
-    .addClass('card project-card draggable my-3')
-    .attr('data-project-id', project.id);
+    .addClass('card task-card draggable my-3')
+    .attr('data-task-id', project.id);
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -43,10 +46,27 @@ function renderTaskList() {
         doneList.append(createTaskCard(task));
     }
   }
+  $('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+    helper: function (e) {
+      // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+      // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
 
-  
+
 }
 window.addEventListener("load", renderTaskList);
+
+
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {
   const form = document.getElementById("taskForm");
@@ -110,7 +130,7 @@ $(document).ready(function () {
     changeYear: true,
   });
 
-  // ? Make lanes droppable
+  
   $('.lane').droppable({
     accept: '.draggable',
     drop: handleDrop,
